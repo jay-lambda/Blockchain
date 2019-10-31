512e4435ce4d6d14249c43f8363d875d5b2da74b
JaySebagh <jaysebagh@gmail.com>
2019-10-31 15:45:42 -0700

    updated

--- basic_wallet_p/lcw/src/App.js
+++ basic_wallet_p/lcw/src/App.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import './App.css';
+import TransactionForm from './components/TransactionForm'
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      user: "",
+      from: "",
+      to: "",
+      amount: 0,
     }
+  }
+
+  transaction = e => {
+    e.preventDefault();
+    
+    const newTransaction = {
+      from: this.from,
+      to: this.to,
+      amount: this.amount
+    }
+
+    this.setState({
+      from: "",
+      to: "",
+      amount: 0,
+    })
   }
 
   handleChanges = e => {
     this.setState({
+      ...this.state,
       [e.target.name]: e.target.value
     })
   }
@@ -19,12 +39,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1>Lambda Coin Wallet</h1>
-        <h3>User: {this.state.user}</h3>
-        <input
-          placeholder="Update User"
-          onChange={this.handleChanges}
-          name="user"
-        />
+        <TransactionForm transaction={this.transaction} handleChanges={this.handleChanges} {...this.state}/>
       </div>
     );
   }

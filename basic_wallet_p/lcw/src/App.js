ee05113c0afdbe1f602c7f40c396259be433d5a1
JaySebagh <jaysebagh@gmail.com>
2019-10-31 15:25:02 -0700

    started

--- basic_wallet_p/lcw/src/App.js
+++ basic_wallet_p/lcw/src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>spooky scary skeletons</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: "",
+    }
+  }
+
+  handleChanges = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Lambda Coin Wallet</h1>
+        <h3>User: {this.state.user}</h3>
+        <input
+          placeholder="Update User"
+          onChange={this.handleChanges}
+          name="user"
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
